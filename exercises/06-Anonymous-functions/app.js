0abc451cfac73539daa0d90e4a1b1ad6ba8015e1
SergioCoto <coto.sergio@gmail.com>
2021-03-07 01:25:44 +0000

    realizados

--- exercises/06-Anonymous-functions/app.js
+++ exercises/06-Anonymous-functions/app.js
@@ -4,3 +4,7 @@ let multy = function(a,b){
 //Dont edit anything ABOVE this line
 
 //your code here
+var returnValue=multy(324234,47);
+console.log(returnValue);
+
+console.log (multy(324234,47));
--- exercises/11-Remove-vowels/app.js
+++ exercises/11-Remove-vowels/app.js
@@ -1,11 +1,25 @@
+const rapid = (myString) => { //entra la palabra John
+let consonants = []; //Creo un nuevo array donde almacenare mis consonantes
+for(let position in myString){ //inicio iterador for en donde creo la variable position que hace referencia a myString = John
+//position = 0
+//position = 1
+//position = 2
+//John[0] = J
+//John[1] = o
+//John[2] = h
+if(['a','e','i','o','u'].includes(myString[position].toLowerCase()) == false) {
 
-const rapid = (myString) => {
-    let consonants = [];
-    for(let position in myString){
-        if(['a','e','i','o','u'].includes(myString[position]) == false)
-            consonants.push(myString[position])
-    }
-    return consonants.join('');
+consonants.push(myString[position].toUpperCase()) //Array o arreglo
+//[].push(J.toUpperCase() = J)
+//['J'].push(h.toUpperCase() = H)
+//['J', 'H']
+}
+//['J', 'H']
+}
+return consonants.join(''); // Array transformado en string
+//['J'] => 'J'
+//['J'] => 'J'
+//['J', 'H'] => 'JH'
 }
 
 // From this line up Do not change code below
